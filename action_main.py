80d49263bc9557b58948582e5a9ba4a8c42d9ccc
talkingtoaster <xz.xuezhen@gmail.com>
2022-01-15 17:32:05 +0800

    update

--- action_main.py
+++ action_main.py
@@ -1,4 +1,69 @@
-import imp
+import os
+import json
+import asyncio
+
+from bili.login import BiliUser
+from bili.smallheart import SmallHeartTask
+from bili.dailyclockin import DailyClockIn
+from push import serverchan
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
 
 
-import index
+DEBUG = 1
+
+
+async def run(user: BiliUser):
+    await user.login()
+    await SmallHeartTask(user).do_work()
+    await DailyClockIn(user).do_work()
+    await user.session.close()
+    message = f"用户:{user.uname}({user.uid})\n" + "\n".join(user.message)
+    if user.message_err:
+        message += "\n错误日志:\n" + "\n".join(user.message_err)
+    return message + "\n\n"
+
+
+def get_config():
+    if DEBUG:
+        with open("./local_config.json", "r") as f:
+            config = json.loads(f.read())
+            print("===============config===============")
+            print(config)
+            print(config['sendkey'])
+    else:
+        config = {
+            "users": [
+                {
+                    "cookie": os.getenv("cookie") or "",
+                    "ruid": int(os.getenv("ruid")),
+                }
+            ],
+            "sendkey": os.getenv("sendkey") or "",
+        }
+    return config
+
+
+def main():
+    config = get_config()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    sendkey = config["sendkey"]
+    message = ""
+
+    for u in config["users"]:
+        if u["cookie"] == "":
+            continue
+        try:
+            user = BiliUser(u["cookie"], u["ruid"])
+            message += loop.run_until_complete(run(user))
+        except Exception as e:
+            message += f"{e}\n"
+    print(message)
+    if sendkey:
+        loop.run_until_complete(serverchan.push_message(sendkey, message))
+
+
+if __name__ == "__main__":
+    main()
--- bili/login.py
+++ bili/login.py
@@ -1,3 +1,4 @@
+from email import header
 import re
 import uuid
 import json
@@ -36,11 +37,22 @@ class BiliUser:
         self.cookie = self.check_cookie(cookie)
         self.ruid = ruid
 
-        self.headers = {
-            "Referer": "https://live.bilibili.com",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/83.0.4103.116 Safari/537.36",
-            "Cookie": self.cookie,
+        self.headers = headers = {
+            'authority': 'api.bilibili.com',
+            'pragma': 'no-cache',
+            'cache-control': 'no-cache',
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'sec-fetch-site': 'none',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-user': '?1',
+            'sec-fetch-dest': 'document',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'cookie': self.cookie,
         }
         self.session = aiohttp.ClientSession(headers=self.headers)
         self.message_err = []  # 错误信息
@@ -56,12 +68,15 @@ class BiliUser:
         if "LIVE_BUVID=" in cookie and "bili_jct=" in cookie and "DedeUserID=" in cookie:
             if cookie.strip()[-1] != ";":
                 cookie += cookie.strip() + ";"
-            self.uid = re.search(r"DedeUserID=([^;]+);", cookie).group(1).strip()
-            self.buvid = re.search(r"LIVE_BUVID=([^;]+);", cookie).group(1).strip()
-            self.csrf = re.search(r"bili_jct=([0-9a-zA-Z]{32})", cookie).group(1).strip()
+            self.uid = re.search(
+                r"DedeUserID=([^;]+);", cookie).group(1).strip()
+            self.buvid = re.search(
+                r"LIVE_BUVID=([^;]+);", cookie).group(1).strip()
+            self.csrf = re.search(
+                r"bili_jct=([0-9a-zA-Z]{32})", cookie).group(1).strip()
             return cookie
         else:
-            message_err = "cookie无效,重新抓取cookie后重试"
+            message_err = "缺少关键键值对，cookie无效,重新抓取cookie后重试"
             logger.error(message_err)
             raise WebApiRequestError(message_err)
 
@@ -90,25 +105,34 @@ class BiliUser:
         """
         await self.check_version()
         url = "https://api.bilibili.com/nav"
-        res = await self.session.post(url)
-        login_data = await res.json()
-        if res.status == 200 and login_data["code"] == 0:
-            self.uname = login_data["data"]["uname"]
-            logger.info(
-                "用户: {} (UID:{})登录成功".format(
-                    login_data["data"]["uname"], login_data["data"]["mid"]
+        res = await self.session.get(url, headers=self.headers)
+        if res.status == 200:
+            code = res.status
+            text = await res.text()
+            print("login200_code:", code)
+            print("login200_text:", text)
+            login_data = await res.json()
+            if login_data["code"] == 0:
+                self.uname = login_data["data"]["uname"]
+                logger.info(
+                    "用户: {} (UID:{})登录成功".format(
+                        login_data["data"]["uname"], login_data["data"]["mid"]
+                    )
                 )
-            )
-            try:
-                sign = await WebApi.do_sign(self.session)
-                message = f"直播区签到成功(本月签到天数:{sign['hadSignDays']}/{sign['allDays']})"
-                logger.info(message)
-                self.message.append(message)
-            except WebApiRequestError as e:
-                message_err = f"直播区签到失败: {e}"
-                logger.error(message_err)
-                self.message_err.append(message_err)
+                try:
+                    sign = await WebApi.do_sign(self.session)
+                    message = f"直播区签到成功(本月签到天数:{sign['hadSignDays']}/{sign['allDays']})"
+                    logger.info(message)
+                    self.message.append(message)
+                except WebApiRequestError as e:
+                    message_err = f"直播区签到失败: {e}"
+                    logger.error(message_err)
+                    self.message_err.append(message_err)
         else:
             message_err = "登录失败,重新抓取cookie后重试"
             logger.error(message_err)
+            code = await res.status
+            text = await res.text()
+            logger.error("login_code:", code)
+            logger.error("login_text:", text)
             raise WebApiRequestError(message_err)
